f1ec1edfb31145be436f55ef4d4971d6dfed3f51
alumno <alumno@a209p34.mshome.net>
2019-12-03 18:00:25 +0100

    Commit examen 2....

--- src/fp/daw/examen/Ejercicio1.java
+++ src/fp/daw/examen/Ejercicio1.java
@@ -1,5 +1,9 @@
 package fp.daw.examen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ejercicio1 {
 
 	/* 
@@ -30,8 +34,95 @@ public class Ejercicio1 {
 	 *     Sólo se permite el uso de recursos del lenguaje Java vistos en las unidades 2 y 3.
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		int numero=0;
+		String prueba="Prueba";
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Introduzca numero a transformar.");
+		String cadena=in.readLine();
+		numero= Integer.parseInt(cadena);
+			System.out.print("ultimo numero" + cadena.charAt(cadena.length()-1));
+		while((numero>3000 || numero<0)) {
+			
+			System.out.println("ERROR, introduce un número menor de 3000.");
+			System.out.println("Introduzca numero a transformar.");
+			numero=Integer.parseInt(in.readLine());
+			//	
+		}
+		
+		for(int i =0;i<cadena.length();i++) {
+			
+			prueba+= mifuncion(cadena.charAt(i),i);
+			System.out.println(" " +cadena.charAt(i) + " ");
+		}
+			
+		System.out.println(prueba);
+		
+	}
+
+	 static String mifuncion(char x,int j) {
+		 String valor="";
+		 String valor1="",valor2="",valor3="";
+		 
+		 if(j==0) {
+			 valor1="M";
+		 }
+		 else if(j==1) {
+			 valor1="C";
+			 valor2="D";
+			 valor3="M";
+		 }
+		 else if(j==2) {
+			 valor1="X";
+			 valor2="L";
+			 valor3="C";
+		 }
+		 else if(j==3) {
+			 valor1="I";
+			 valor2="V";
+			 valor3="X";
+		 }
+		 System.out.println(valor1 + "primera " + " " + valor2+ " " + valor3 + "charAT "+ x + "\norden " + j);
+		
+		 switch(x) {
+		case 1:
+			valor+=valor1;
+			break;
+		case 2:
+			valor+=valor1 + "" +valor1;
+			break;
+		case 3:
+			valor+=valor1 + "" + valor1 + "" + valor1;
+			break;
+		case 4:
+			valor+= valor1 + "" + valor2;
+			break;
+			
+		case 5:
+			valor+=valor2;
+			break;
+			
+
+		case 6:
+			valor+= valor2 + "" +valor1;
+			break;
+
+		case 7:
+			valor+=valor2 + ""+ valor1 +"" + valor1;
+			break;
+
+		case 8:
+			valor+= valor2 + "" + valor1 + " " + valor1 +"" + valor1;
+			break;
+
+		case 9:
+			valor+=valor1+ "" + valor3;
+			break;
+		}
+		
+		return valor;
 	}
 	
 }
--- src/fp/daw/examen/Ejercicio2.java
+++ src/fp/daw/examen/Ejercicio2.java
@@ -1,5 +1,9 @@
 package fp.daw.examen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ejercicio2 {
 
 	/*
@@ -18,8 +22,46 @@ public class Ejercicio2 {
 	 * Sólo se permite el uso de recursos del lenguaje Java vistos en las unidades 2 y 3.
 	 */
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		String prueba="";
+		int num1;
+		int num2;
+		int mayor=0;
+		int menor=0;
+		int suma=0;
+	
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Introduzca numero 1.");
+		num1 = Integer.parseInt(in.readLine());
+		System.out.println("Introduzca numero 2.");
+		num2 = Integer.parseInt(in.readLine());
+		
+		if(num1<num2) {
+			mayor=num2;
+			menor=num1;
+		}
+		else if(num1>num2) {
+			mayor=num1;
+			menor=num2;
+		}
+		else {
+			System.out.println("Son iguales.");
+		}
+		
+		for(int i=menor;i<=mayor;i++) {		
+			if(i%2!=0) {
+				suma+=i;
+				prueba+=i+" ";
+				
+			}
+		}
+		
+		System.out.println(prueba);
+		System.out.println("La suma entre " + menor + " y " + mayor + " es " + suma);
+		}
 		
 	}
 
-}
+
